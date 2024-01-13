1b56207124e9c2d5038c58f87c918de80a0ea91b
ludan <466568964@qq.com>
2024-01-13 11:22:39 +0800

    新增demo03

--- src/pages/bigScreen/demo03/index.vue
+++ src/pages/bigScreen/demo03/index.vue
@@ -1,5 +1,22 @@
 <template>
+    <div style="height: 100vh;">
+        <div style="background-color: aliceblue;height: 80px;">
+            head
+        </div>
+        <div
+            style="height: calc(100vh - 110px); padding: 15px; display: flex; flex-wrap: wrap; justify-content: space-between;">
+            <div class="box" style="background-color: blanchedalmond; width: calc(30% - 15px); height: 42%;">
+                1
+            </div>
+            <div class="box" style="background-color: blanchedalmond; width: calc(40% - 15px); height: 42%;">
+                2
+            </div>
+            <div class="box" style="background-color: blanchedalmond; width: calc(30%); height: 42%;">
+                3
+            </div>
 
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -7,7 +24,5 @@
 </script>
 
 <style scoped>
-
-
-
+.box {}
 </style>
