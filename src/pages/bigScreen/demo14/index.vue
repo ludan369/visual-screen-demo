f667c168247a259bbfbff61b86f6d6606c4f5973
ludan <466568964@qq.com>
2024-10-11 17:40:14 +0800

    demo14

--- src/pages/bigScreen/demo14/index.vue
+++ src/pages/bigScreen/demo14/index.vue
@@ -1,5 +1,16 @@
 <template>
-demo14
+    <div class="main">
+        <div class="head clearfix">
+            <h1 class="pulll_left">xxxxx数据监控平台</h1>
+            <div class="menu menu2 pulll_left">
+                <ul>
+                    <li v-for="index in 6" :key="index">
+                        <a> 标题样式</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +18,5 @@ demo14
 </script>
 
 <style scoped>
-/* @import url('./style/index.less'); */
+@import url('./style/index.less');
 </style>
